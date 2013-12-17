5f0f948077b0ebf94b13d44d909ecb0f83e550fb
Nick Doherty <nickdoherty@gmail.com>
2013-12-17 20:37:38 +1100

    added devise controller

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -13,12 +13,16 @@ class StreamsController < ApplicationController
     @stream = current_user.streams.new(params[:stream].except(:invited_user))
 
     invited_user = User.find_by_email(params[:stream][:invited_user])
-
+    if invited_user.nil?
+      @pass = rand(1000000)
+      invited_user = User.create(:email => params[:stream][:invited_user], :password => @pass, :password_confirmation => @pass)
+    end
     @stream.users << invited_user
+    @stream.users << current_user
     if @stream.save
       UserMailer.invitation_email(invited_user, params[:stream][:message], current_user, @stream).deliver
       # redirect
-      redirect_to root_path
+      redirect_to stream_path(@stream)
     else
       render :new
     end

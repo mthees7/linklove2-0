e8cd635e694b97b7435d9d0612d0f42c8407fac7
Nick Doherty <nickdoherty@gmail.com>
2013-12-19 10:36:42 +1100

    removed gutentag tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131218232605) do
+ActiveRecord::Schema.define(:version => 20131218042141) do
 
   create_table "memberships", :force => true do |t|
     t.integer  "user_id"
